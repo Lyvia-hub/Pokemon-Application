ab1f63590257d5718826a649a934903a3aeba8f4
Lyvia Blay <lyviablay@gmail.com>
2020-08-07 19:17:19 -0400

    addition of a background into the cards when the user is hovering

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -5,28 +5,51 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class BorderCardDirective {
 
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 230;
+  private backgroundColor: string = 'black';
+  private initialBackgroundColor: string = 'none';
+  private initialTextColor: string = 'black';
+  private textColor: string = 'white';
+
   constructor(private el: ElementRef) {
-    this.setBorder('#f5f5f5');
-    this.setHeight(250);
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
+    this.setBackground(this.backgroundColor);
+    this.settextColor(this.textColor);
   }
 
   @Input('appBorderCard') borderColor: string; // alias
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder(this.borderColor || '#009688');
+    // utilisation de l'alias borderColor
+    this.setBorder(this.borderColor || this.defaultColor);
+    this.setBackground(this.backgroundColor);
+    this.settextColor(this.textColor);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
+    this.setBackground(this.initialBackgroundColor);
+    this.settextColor(this.initialTextColor);
   }
 
   private setBorder(color: string) {
-    let border = 'solid 4px ' + color;
+    const border = 'solid 4px ' + color;
     this.el.nativeElement.style.border = border;
   }
 
   private setHeight(height: number) {
     this.el.nativeElement.style.height = height + 'px';
+  }
+
+  private setBackground(background: string) {
+    this.el.nativeElement.style.background = background;
+  }
+
+  private settextColor(color: string) {
+    this.el.nativeElement.style.color = color;
   }
 
 
