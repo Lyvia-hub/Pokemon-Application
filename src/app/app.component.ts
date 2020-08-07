477857ddc5f447e448283952daaf6202084f0806
Lyvia Blay <lyviablay@gmail.com>
2020-08-07 18:47:11 -0400

    border card style with borderCard Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit {
+  public pokemons: Pokemon[];
+  public title = 'Liste des Pokémons';
+
+
+  // data initialisation
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    alert(`Vous avez cliqué sur ${pokemon.name}`);
+  }
 }
