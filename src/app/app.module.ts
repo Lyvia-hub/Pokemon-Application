c193608aa49523af44473a4af211e35808bfb1f2
Lyvia Blay <lyviablay@gmail.com>
2020-08-19 12:39:49 -0400

    Addition of angular-in-memory-web-api into package.json and import of HttpClientModule into app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { PokemonsModule } from './pokemons/pokemons.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +17,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     PokemonsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
